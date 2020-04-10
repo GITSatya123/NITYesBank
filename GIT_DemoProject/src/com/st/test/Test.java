f661a296f6c4572a27ff1db2ceb56fb2ac94ef71
TL <DEV2@gmail.com>
2020-04-10 19:36:32 +0530

    Dev1 modified Test.java

--- GIT_DemoProject/src/com/st/test/Test.java
+++ GIT_DemoProject/src/com/st/test/Test.java
@@ -1,5 +1,11 @@
 package com.st.test;
 
+import com.st.bean.Utility;
+
 public class Test {
 
+	public static void main(String[] args) {
+		Utility u=new Utility();
+		System.out.println(u.getSysDate());
+	}
 }
