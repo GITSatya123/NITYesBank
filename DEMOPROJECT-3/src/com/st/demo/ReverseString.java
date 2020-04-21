b3dbe3b05ab2744f14fea7599aafdd40a7ae9679
TL <DEV2@gmail.com>
2020-04-21 22:44:06 +0530

    import by dev1

---
+++ DEMOPROJECT-3/src/com/st/demo/ReverseString.java
@@ -0,0 +1,17 @@
+package com.st.demo;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		
+		String str= "Satyajit";
+		StringBuilder sb= new StringBuilder();
+		sb.append(str);
+		sb=sb.reverse();
+			System.out.println(sb);
+		}
+	
+
+	}
+
+
